f8f52e0585df035c803be19d2a26d968b4e71684
katya <katyamit5@gmail.com>
2023-11-27 16:11:38 +0300

    add buttons

--- src/main/java/department/servlet/IndexServlet.java
+++ src/main/java/department/servlet/IndexServlet.java
@@ -6,13 +6,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+//@WebServlet("/")
+//public class IndexServlet extends HttpServlet {
+//
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        resp.sendRedirect(req.getContextPath() + "/employee");
+//    }
+//}
 @WebServlet("/")
 public class IndexServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        getServletContext().getRequestDispatcher("/hello").forward(req, resp);
-        resp.sendRedirect(req.getContextPath() + "/employee");
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        out.println("<html><body>");
+        out.println("<h1>Welcome!</h1>");
+        out.println("<form action=\"department\" method=\"get\">");
+        out.println("<input type=\"submit\" value=\"Department\">");
+        out.println("</form>");
+        out.println("<form action=\"employee\" method=\"get\">");
+        out.println("<input type=\"submit\" value=\"Employee\">");
+        out.println("</form>");
+        out.println("</body></html>");
+
+        out.close();
     }
 }
+
